dca2d8bf36b01dd8ed560201e8cd0e4876a739dc
Han xuesi <419732502@qq.com>
2020-07-15 18:36:05 +0800

    添加三个警示标语

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -35,6 +35,23 @@ public class PublishController {
             @RequestParam("tag") String tag,
             HttpServletRequest request,
             Model model) {
+
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if (description == null || description == "") {
+            model.addAttribute("error", "内容不能为空");
+            return "publish";
+        }
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
         User user = null;
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
