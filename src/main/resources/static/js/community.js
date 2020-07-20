5edb2c09893004ef22f17287a4d7c458136757ca
Han Xuesi  bjb <419732502@qq.com>
2020-07-20 21:54:41 +0800

    实现不刷新回复详情页面，跳转到另一个页面登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,9 +14,16 @@ function post() {
             if (response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=045a567aafd24955e8f2&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
-            console.log(response);
         },
         dataType: "json"
     });
