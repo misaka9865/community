10256afb443ebd3fcacf27077050538c668ba07f
Han xuesi <419732502@qq.com>
2020-07-22 12:17:15 +0800

    完成二级评论样式，准备完成服务器端功能

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -5,7 +5,7 @@ function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
 
-    if (!content){
+    if (!content) {
         alert("不能回复空内容");
         return;
     }
@@ -42,6 +42,25 @@ function post() {
  * 展开二级评论
  */
 function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-" + id);
 
+    //获取一下二级评论的展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse) {
+
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    } else {
+
+        //展开二级评论
+        comments.addClass("in");
+
+        //标记二级评论展开状态
+        e.setAttribute("data-collapse", "in");
+        e.classList.add("active");
+    }
 
 }
